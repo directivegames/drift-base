67247accba3b3032765f61827fc4f9fa92d959de
Einar Th. Einarsson <einsi@directivegames.com>
2021-03-01 16:19:37 +0000

    WIP for syncing to home: draft version of matchmaking start

--- driftbase/flexmatch.py
+++ driftbase/flexmatch.py
@@ -1,20 +1,84 @@
 
+import boto3
 from flask import g
+from driftbase.parties import get_player_party, get_party_members
 
 NUM_VALUES_FOR_LATENCY_AVERAGE = 3
+AWS_REGION = "eu-west-1"  #FIXME: Get from config or environment
+MATCHMAKING_CONFIGURATION_NAME = "PerseusHilmarMatchmaker" #FIXME: Get from config or environment
+REDIS_TTL = 1800
+
+gamelift_client = boto3.client("gamelift", region_name=AWS_REGION)
 
 
 def update_player_latency(player_id, latency_ms):
-    redis = g.redis.conn
     latency_key = _make_player_latency_key(player_id)
-    redis.lpush(latency_key, latency_ms)
+    g.redis.conn.lpush(latency_key, latency_ms)
 
 def get_player_latency_average(player_id):
     latency_key = _make_player_latency_key(player_id)
-    redis = g.redis.conn
-    redis.ltrim(latency_key, 0, NUM_VALUES_FOR_LATENCY_AVERAGE - 1)  # only use the 5 latest value
-    values = [float(i) for i in redis.lrange(latency_key, 0, -1)]
-    return sum(values) / min(NUM_VALUES_FOR_LATENCY_AVERAGE, len(values))
+    g.redis.conn.ltrim(latency_key, 0, NUM_VALUES_FOR_LATENCY_AVERAGE - 1)
+    values = [float(i) for i in g.redis.conn.lrange(latency_key, 0, -1)]
+    return sum(values) / min(NUM_VALUES_FOR_LATENCY_AVERAGE, len(values)) # FIXME: default value if no values reported?
+
+def upsert_flexmatch_search(player_id):
+    fm_state = _get_matchmaking_status_for_player(player_id)
+    if fm_state:
+        # FIXME: enumerate and handle all real ticket statuses
+        if fm_state["status"] == "searching":
+            return fm_state["ticket_id"]
+
+    # Generate a list of players relevant to the request; this is the list of online players in the party if the player belongs to one, otherwise the list is just the player
+    player_party_id = get_player_party(player_id)
+    if player_party_id:
+        member_ids = get_party_members(player_party_id)
+    else:
+        member_ids = [player_id]
+
+    response = gamelift_client.start_matchmaking(
+        ConfigurationName = MATCHMAKING_CONFIGURATION_NAME,
+        Players = [
+            {
+                "PlayerId": str(member_id),
+                "PlayerAttribute": {"skill": {"N": 50}},
+                "LatencyInMs": {AWS_REGION: int(get_player_latency_average(member_id))} # FIXME: check if this is the intended mapping, i.e. look up the rule who consumes this
+            }
+            for member_id in member_ids
+        ],
+    )
+    if "MatchmakingTicket" not in response:
+        return {
+            "error": "No 'MatchmakingTicket' in ticket response, this is weird",
+            "response_for_debug": response,
+        }
+
+    ticket = response["MatchmakingTicket"]
+    ticket_id = ticket["TicketId"]
+    ticket_status = ticket["Status"]
+    fm_state = {
+        "ticked_it": ticket_id,
+        "status": ticket_status
+    }
+    g.redis.conn.set(_make_matchmaking_state_key(player_id), fm_state)
+
+    # FIXME: Generate matchmaking status messages for all players in member_ids
+    return {"ticket_id": ticket_id}
+
 
 def _make_player_latency_key(player_id):
     return g.redis.make_key("player:{}:latency:".format(player_id))
+
+def _make_matchmaking_state_key(player_id):
+    player_party_id = get_player_party(player_id)
+    if player_party_id is not None:
+        return g.redis.make_key("party:{}:flexmatch:".format(player_party_id))
+    return g.redis.make_key("player:{}:flexmatch:".format(player_id))
+
+def _get_matchmaking_status_for_player(player_id):
+    """
+    Format of status:
+    {
+        "ticket_id" : <some id>
+        "status" : <some status string?>
+    """
+    return g.redis.conn.hgetall(_make_matchmaking_state_key(player_id))
--- driftbase/tests/test_flexmatch.py
+++ driftbase/tests/test_flexmatch.py
@@ -1,7 +1,9 @@
-from driftbase.utils.test_utils import BaseCloudkitTest
-from six.moves import http_client
 
-class PlayersTest(BaseCloudkitTest):
+from six.moves import http_client
+from driftbase.utils.test_utils import BaseCloudkitTest
+
+class FlexMatchTest(BaseCloudkitTest):
+        
     def test_patch_latency(self):
         self.auth()
         flexmatch_url = self.endpoints["flexmatch"]
@@ -11,3 +13,9 @@ class PlayersTest(BaseCloudkitTest):
             response = self.patch(flexmatch_url, data={'latency_ms': latency}, expected_status_code=http_client.OK)
             reported_avg = response.json()["latency_avg"]
             self.assertEqual(reported_avg, expected_avg[i])
+
+    def test_start_matchmaking_without_a_party(self):
+        self.auth()
+        flexmatch_url = self.endpoints["flexmatch"]
+        breakpoint()
+        response = self.post(flexmatch_url)
