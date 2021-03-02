13765220354825d7f4cbeec95ca0f4c7660f502b
Einar Th. Einarsson <einsi@directivegames.com>
2021-03-02 16:44:38 +0000

    make latency averages be region specific

--- driftbase/tests/test_flexmatch.py
+++ driftbase/tests/test_flexmatch.py
@@ -7,15 +7,15 @@ class FlexMatchTest(BaseCloudkitTest):
     def test_patch_latency(self):
         self.auth()
         flexmatch_url = self.endpoints["flexmatch"]
-        latencies = [1.0, 2.0, 3.0, 4.0, 5.0]
-        expected_avg = [1.0, 1.5, 2.0, 3.0, 4.0]
+        latencies = [1.0, 2.0, 3.0, 4.0, 5.0, 10.7]
+        expected_avg = [1, 1, 2, 3, 4, 6] # We expect unrounded integers representing the average of the last 3 values
         for i, latency in enumerate(latencies):
-            response = self.patch(flexmatch_url, data={'latency_ms': latency}, expected_status_code=http_client.OK)
-            reported_avg = response.json()["latency_avg"]
+            response = self.patch(flexmatch_url, data={'latency_ms': latency, "region": "eu-west-1" }, expected_status_code=http_client.OK)
+            reported_avg = response.json()["latency_avg"]["eu-west-1"]
             self.assertEqual(reported_avg, expected_avg[i])
 
     def test_start_matchmaking_without_a_party(self):
         self.auth()
         flexmatch_url = self.endpoints["flexmatch"]
-        breakpoint()
         response = self.post(flexmatch_url)
+        breakpoint()
