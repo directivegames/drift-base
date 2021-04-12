78f769bfcddef9a8de7edd2184bddd5f00ec18cb
Einar Th. Einarsson <einsi@directivegames.com>
2021-04-12 15:45:29 +0000

    Renamed the endpoint and api class from flexmatch/FlexMatchAPI to matchmaking/MatchmakingAPI. Implementation is still called flexmatch though. Split up tests into API tests and flexmatch tests.

--- driftbase/tests/test_matchmaking.py
+++ driftbase/tests/test_matchmaking.py
@@ -10,60 +10,63 @@ import sys
 
 REGION = "eu-west-1"
 
-class FlexMatchTest(BaseCloudkitTest):
+class TestMatchmakingAPI(BaseCloudkitTest):
     def test_patch_api(self):
         self.make_player()
-        flexmatch_url = self.endpoints["flexmatch"]
+        matchmaking_url = self.endpoints["matchmaking"]
         with patch.object(flexmatch, 'update_player_latency', return_value=None):
             with patch.object(flexmatch, 'get_player_latency_averages', return_value={}):
-                self.patch(flexmatch_url, expected_status_code=http_client.UNPROCESSABLE_ENTITY)
-                self.patch(flexmatch_url, data={'latency_ms': 123, "region": REGION}, expected_status_code=http_client.OK)
+                self.patch(matchmaking_url, expected_status_code=http_client.UNPROCESSABLE_ENTITY)
+                self.patch(matchmaking_url, data={'latency_ms': 123, "region": REGION}, expected_status_code=http_client.OK)
 
     def test_post_api(self):
         self.make_player()
+        matchmaking_url = self.endpoints["matchmaking"]
         with patch.object(flexmatch, 'upsert_flexmatch_ticket', return_value={}):
-            self.post(self.endpoints["flexmatch"], expected_status_code=http_client.UNPROCESSABLE_ENTITY)
-            response = self.post(self.endpoints["flexmatch"], data={"matchmaker": "test"}, expected_status_code=http_client.OK)
+            self.post(matchmaking_url, expected_status_code=http_client.UNPROCESSABLE_ENTITY)
+            response = self.post(matchmaking_url, data={"matchmaker": "test"}, expected_status_code=http_client.OK)
             self.assertDictEqual(response.json(), {})
 
     def test_get_api(self):
         self.make_player()
+        matchmaking_url = self.endpoints["matchmaking"]
         with patch.object(flexmatch, 'get_player_ticket', return_value={}):
-            response = self.get(self.endpoints["flexmatch"], expected_status_code=http_client.OK)
+            response = self.get(matchmaking_url, expected_status_code=http_client.OK)
             self.assertDictEqual(response.json(), {})
 
+class FlexMatchTest(BaseCloudkitTest):
     def test_update_latency_returns_correct_averages(self):
         self.make_player()
-        flexmatch_url = self.endpoints["flexmatch"]
+        matchmaking_url = self.endpoints["matchmaking"]
         latencies = [1.0, 2.0, 3.0, 4.0, 5.0, 10.7]
         expected_avg = [1, 1, 2, 3, 4, 6] # We expect unrounded integers representing the average of the last 3 values
         for i, latency in enumerate(latencies):
-            response = self.patch(flexmatch_url, data={'latency_ms': latency, "region": REGION}, expected_status_code=http_client.OK)
+            response = self.patch(matchmaking_url, data={'latency_ms': latency, "region": REGION}, expected_status_code=http_client.OK)
             self.assertEqual(response.json()[REGION], expected_avg[i])
 
     def test_start_matchmaking(self):
         self.make_player()
-        flexmatch_url = self.endpoints["flexmatch"]
+        matchmaking_url = self.endpoints["matchmaking"]
         with patch.object(boto3, 'client', MockGameLiftClient):
-            response = self.post(flexmatch_url, data={"matchmaker": "test"}, expected_status_code=http_client.OK).json()
+            response = self.post(matchmaking_url, data={"matchmaker": "test"}, expected_status_code=http_client.OK).json()
             self.assertTrue(response["status"] == "QUEUED")
             self.assertEqual(response["ticket_id"], 123)
 
     def test_start_matchmaking_doesnt_modify_ticket_if_same_player_reissues_request(self):
         self.make_player()
-        flexmatch_url = self.endpoints["flexmatch"]
+        matchmaking_url = self.endpoints["matchmaking"]
         with patch.object(boto3, 'client', MockGameLiftClient):
-            response1 = self.post(flexmatch_url, data={"matchmaker": "test"}, expected_status_code=http_client.OK).json()
+            response1 = self.post(matchmaking_url, data={"matchmaker": "test"}, expected_status_code=http_client.OK).json()
             first_id = response1["debug_id"]
-            response2 = self.post(flexmatch_url, data={"matchmaker": "test"}, expected_status_code=http_client.OK).json()
+            response2 = self.post(matchmaking_url, data={"matchmaker": "test"}, expected_status_code=http_client.OK).json()
             second_id = response2["debug_id"]
             self.assertEqual(first_id, second_id)
 
     def test_start_matchmaking_creates_event(self):
         self.make_player()
-        flexmatch_url = self.endpoints["flexmatch"]
+        matchmaking_url = self.endpoints["matchmaking"]
         with patch.object(boto3, 'client', MockGameLiftClient):
-            self.post(flexmatch_url, data={"matchmaker": "test"}, expected_status_code=http_client.OK).json()
+            self.post(matchmaking_url, data={"matchmaker": "test"}, expected_status_code=http_client.OK).json()
             notification, message_number = self.get_player_notification("matchmaking", "StartedMatchMaking")
             self.assertIsInstance(notification, dict)
             self.assertTrue(notification["event"] == "StartedMatchMaking")
@@ -81,7 +84,7 @@ class FlexMatchTest(BaseCloudkitTest):
         self.patch(notification['invite_url'], data={'inviter_id': host_id}, expected_status_code=http_client.OK).json()
         # Let member start matchmaking, host should be included in the ticket
         with patch.object(boto3, 'client', MockGameLiftClient):
-            response = self.post(self.endpoints["flexmatch"], data={"matchmaker": "test"}, expected_status_code=http_client.OK).json()
+            response = self.post(self.endpoints["matchmaking"], data={"matchmaker": "test"}, expected_status_code=http_client.OK).json()
             players = response["Players"]
             self.assertEqual(len(players), 2)
             expected_players = {host_id, member_id}
