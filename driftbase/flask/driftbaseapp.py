2a50e8b7f9cd89bcbc2e02a536806963f59b22fb
Andreas Axelsson <axl@directivegames.com>
2021-09-03 21:52:16 +0800

    Experiment with running periodic tasks as gevent threads

--- driftbase/flask/driftbaseapp.py
+++ driftbase/flask/driftbaseapp.py
@@ -1,6 +1,7 @@
 """
 Flask app with gevent monkey patching.
 """
+import gevent
 from gevent import monkey
 monkey.patch_all()
 from psycogreen.gevent import patch_psycopg
@@ -14,3 +15,14 @@ if os.environ.get('ENABLE_DATADOG_APM', '0') == '1':
 
 from drift.flaskfactory import drift_app
 app = drift_app()
+
+def foo():
+    from driftconfig.util import get_default_drift_config
+    from time import sleep
+    while True:
+        print("checking...")
+        config = get_default_drift_config()
+        print(config.get_table('tenants').find({'tenant_name': 'mw-tenant'}))
+        sleep(2)
+
+gevent.spawn_later(5, foo)
