f93a797f03e69358282c9cde76a0c46a7f6e9633
Einar Th. Einarsson <einsi@directivegames.com>
2024-03-11 13:55:18 +0000

    Only send the login message if there's a user_id and a player_id in the payload as we might be creating a 'naked' identity.  Also include all identities in the message

--- driftbase/auth/authenticate.py
+++ driftbase/auth/authenticate.py
@@ -264,7 +264,13 @@ def authenticate(username, password, automatic_account_creation=True, fallback_u
     )
     cache = UserCache()
     cache.set_all(user_id, ret)
-    current_app.extensions.get('shoutout').message("player_login", **ret)
+    if user_id and player_id:
+        messaga_data = ret.copy()
+        player_identities= []
+        for identity in g.db.query(UserIdentity).filter(UserIdentity.name == username).all():
+            player_identities.append(dict(identity_id=identity.identity_id, identity_type=identity.identity_type, name=identity.name))
+        messaga_data['identities'] = player_identities
+        current_app.extensions.get('shoutout').message("player_login", **messaga_data)
     return ret
 
 
