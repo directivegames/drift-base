972d50e5459ee7f36ccbdb4b0fbc41daa6fc84f2
Einar Th. Einarsson <einsi@directivegames.com>
2024-04-01 16:32:48 +0000

    Remove datadog patching from driftbaseapp, in line with seasons

--- driftbase/flask/driftbaseapp.py
+++ driftbase/flask/driftbaseapp.py
@@ -1,21 +1,12 @@
 """
 Flask app with gevent monkey patching.
 """
-
-from gevent import monkey
-monkey.patch_module("threading")  # noqa: E402
+try:
+    from gevent import monkey
+    monkey.patch_module("threading")  # noqa: E402
+except ImportError:
+    print(f"Failed to patch 'threading' module with gevent monkey patching")
 
 from drift.flaskfactory import drift_app  # Do this first for gevent monkey patching
-import os
-
-# Optionally enable Metrics
-if os.environ.get('ENABLE_DATADOG_METRICS', '0') == '1':
-    from ddtrace.runtime import RuntimeMetrics
-    RuntimeMetrics.enable()
-
-# Optionally enable APM
-if os.environ.get('ENABLE_DATADOG_APM', '0') == '1':
-    import ddtrace
-    ddtrace.patch_all(logging=True)
 
 app = drift_app()
